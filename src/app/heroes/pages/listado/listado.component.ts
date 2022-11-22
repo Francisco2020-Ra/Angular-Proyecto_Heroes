4aafdd2c230d47f6998a312a4688d0ff647dedfa
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-11-21 21:05:27 -0300

    Flex Layout - Resoluciones con @media query

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -5,8 +5,49 @@ import { Heroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
-  ]
+  styles: [`
+
+    
+    .container{
+      display:flex;
+      flex-flow: row wrap;
+      justify-content:center;
+      gap: 20px;
+    }
+    .card-view{
+      width: 15%;
+    }
+    /** El orden de las media query es de mayor a menor */
+    
+    /** siempre que este entre 959 y 1280 ejecuta esta instruccion */
+    @media (max-width: 1280px) {
+      .card-view {
+        width: 20%;
+      }
+    }
+    /** siempre que este entre 0 y 959 ejecuta esta instruccion */
+    @media (max-width: 959px) {
+      .card-view {
+        width: 30%;
+      }
+    }
+
+     /** siempre que este entre 0 y 599 ejecuta esta instruccion */
+    @media (max-width: 599px) {
+      .container{
+        display:flex;
+        flex-flow: column;
+        align-items: center;
+      }
+      .card-view{
+        width: 50%;
+      }
+    }
+
+   
+   
+
+  `]
 })
 export class ListadoComponent implements OnInit {
 
@@ -16,9 +57,9 @@ export class ListadoComponent implements OnInit {
 
   ngOnInit(): void {
     this.heroesService.getHeroes()
-    .subscribe( heroes => {
-      this.heroes = heroes;
-    })
+      .subscribe(heroes => {
+        this.heroes = heroes;
+      })
   }
 
 }
