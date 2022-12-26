2f7d9a19516c584c8aea88684e70a42c14ef73f8
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-12-26 14:02:38 -0300

    Tarea-Ruta heroe y editar heroe

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -9,33 +9,15 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
-    path:'',
+    path: '',
     component: HomeComponent,
     children: [
-      {
-        path: 'listado',
-        component: ListadoComponent
-      },
-      {
-        path:'agregar',
-        component: AgregarComponent
-      },
-      {
-        path: 'editar/:id',
-        component: AgregarComponent
-      },
-      {
-        path:'buscar',
-        component: BuscarComponent
-      },
-      {
-        path: ':id',
-        component: HeroeComponent
-      },
-      {
-        path: '**',
-        redirectTo: 'listado'        
-      }
+      { path: 'listado', component: ListadoComponent },
+      { path: 'agregar', component: AgregarComponent },
+      { path: 'editar/:id', component: AgregarComponent },
+      { path: 'buscar', component: BuscarComponent },
+      { path: ':id', component: HeroeComponent },
+      { path: '**',  redirectTo: 'listado' }
     ]
   }
 ]
