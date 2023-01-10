5f9b2ab75b4f7024e00b2cd08c04098441a203d6
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2023-01-09 21:15:33 -0300

    Diseño de la pantalla para agregar heroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,13 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
+  styles: [`
+  .container{
+    display:flex;
+    flex-direction: row;
+    box-sizing: border-box;
+    gap: 40px;
+  }
+  .container-medium{
+    flex-direction: column;
+    width:50%;
+  }
+  .container-field{
+    width:100%;
+  }
+  .container-line{
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+  }
+ 
+`
   ]
 })
 export class AgregarComponent implements OnInit {
 
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+
+  }
   constructor() { }
 
   ngOnInit(): void {
