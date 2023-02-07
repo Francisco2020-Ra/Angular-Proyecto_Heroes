331528e91c7d390b889ada81cdedf6cd308f0c9a
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2023-02-07 14:28:59 -0300

    Material dialog para cuando se borra un heroe

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -13,6 +13,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 import { ImagenPipe } from './pipes/imagen.pipe';
+import { ConfirmarComponent } from './components/confirmar/confirmar.component';
 
 
 
@@ -27,7 +28,8 @@ import { ImagenPipe } from './pipes/imagen.pipe';
     ListadoComponent,
     HeroeTarjetaComponent,
 
-    ImagenPipe
+    ImagenPipe,
+     ConfirmarComponent
   ],
   imports: [
     CommonModule,
--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -4,6 +4,8 @@ import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmarComponent } from '../../components/confirmar/confirmar.component';
 
 @Component({
   selector: 'app-agregar',
@@ -74,7 +76,8 @@ export class AgregarComponent implements OnInit {
   constructor(private heroeService: HeroesService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private _snackBar: MatSnackBar) { }
+    private _snackBar: MatSnackBar,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
     if(this.router.url.includes('editar')){
@@ -108,12 +111,23 @@ export class AgregarComponent implements OnInit {
   }
   
   borrarHeroe() {
-    this.heroeService.borrarHeroe( this.heroe.id! )
-      .subscribe( resp => {
 
-      this.router.navigate(['/heroes']);
+    const dialog = this.dialog.open(ConfirmarComponent,{
+      width: '250px',
+      data: { ...this.heroe }
+    })
 
-    });
+    dialog.afterClosed().subscribe(
+      result => {
+        if( result ){
+          this.heroeService.borrarHeroe( this.heroe.id! )
+            .subscribe( resp => {
+              this.router.navigate(['/heroes']);
+            }); 
+        }
+      }
+    )
+ 
   }
 
   mostrarSnakbar(mensaje: string){
